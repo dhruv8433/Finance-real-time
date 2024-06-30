3640e073485163ab46d3c13b4e1f8d2493270e37
Varsani2520 <varsaniranjani2520@gmail.com>
2024-06-30 11:21:18 +0530

    env done

--- app/utils/fetchBalance.js
+++ app/utils/fetchBalance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { BALANCE_API_KEY } from '@/app/Config/config';
 
-const BASE_URL = 'https://financialmodelingprep.com/api/v3';
+const BASE_URL = process.env.BALANCE_API_URL;
 
 export const fetchBalance = async (symbol) => {
   const url = `${BASE_URL}/income-statement-growth/AMZN?period=annual&apikey=${BALANCE_API_KEY}`;
