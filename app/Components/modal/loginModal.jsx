698fff8078a347ac1de81e34eb5d08a6d7bfb58b
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-06-30 11:54:45 +0530

    update login modal

--- app/Components/modal/loginModal.jsx
+++ app/Components/modal/loginModal.jsx
@@ -20,6 +20,7 @@ import loginAnimation from "../../lottie-animation/login.json";
 import Toast from "../Toast";
 import Cookies from "js-cookie";
 import { loginUserFailure, loginUserSuccess } from "@/app/Redux/action/action";
+import { loginservice } from "@/app/utils/services/loginService";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
@@ -39,7 +40,7 @@ const LoginPage = () => {
     }
 
     try {
-      const response = await loginService(login.username, login.password);
+      const response = await loginservice(login.username, login.password);
       console.log(response);
       Cookies.set("user", true);
       dispatch(loginUserSuccess(response));
