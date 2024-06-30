a84b5b8853a7ce972990f19e27749f71317a12f8
Varsani2520 <varsaniranjani2520@gmail.com>
2024-06-30 08:28:11 +0530

    auth with redux

---
+++ app/Redux/action/action.js
@@ -0,0 +1,17 @@
+import { LOGIN_USER_FAILURE, LOGIN_USER_SUCCESS, LOGOUT_USER } from "../Reducer/auth";
+
+
+
+
+// auth action
+export const loginUserSuccess = (userData) => ({
+  type: LOGIN_USER_SUCCESS,
+  payload: userData,
+});
+
+export const loginUserFailure = () => ({
+  type: LOGIN_USER_FAILURE,
+});
+export const logoutuser = () => ({
+  type: LOGOUT_USER,
+});
